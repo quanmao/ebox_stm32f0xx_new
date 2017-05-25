4e1babae3ea6a8a8bc3473536318473ab6d3a952
好心情 <cat_li8953@qq.com>
2017-05-25 23:15:03 +0800

    uart
    继承print对象，添加中断附加功能

--- mcu/stm32f0x/ebox/Peripherals/ebox_uart.cpp
+++ mcu/stm32f0x/ebox/Peripherals/ebox_uart.cpp
@@ -5,6 +5,7 @@
   * @version V2.0
   * @date    2016/10/21
   * @brief   
+	1  2017/5/25  移除E_PinBase的构造函数，使用PIN_ID
   ******************************************************************************
   * @attention
   *
@@ -19,55 +20,25 @@
 #include "stm32f0xx_ll_usart.h"
 #include <stdio.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-	
-//uint8_t find(uint16_t pin, uint32_t UARTx,const E_PIN_FUN_T *emap)
-//{
-//		while(!((emap->pin_id == pin) && (emap->fun == UARTx)))
-//		{
-//				if(emap->pin_id == (uint16_t)NC){return (uint8_t)E_NC;}
-//				emap++;
-//		}
-//		return emap->alternate;
-//}
+static uint32_t serial_irq_ids[UART_NUM];  // 保存对象地址，供静态成员识别对象，并访问对象的普通成员
+static uart_irq_handler irq_handler;	   // 声明函数指针变量，指向类的静态成员
 
-//uint8_t Find(uint32_t Periphx,const E_PIN_FUN_T *emap)
-//{
-//		uint8_t i = 0;
-//		while(!((emap+i)->name == Periphx ))
-//		{
-//				if((emap+i)->name == (uint16_t)NC){return (uint8_t)E_NC;}
-//				i++;
-//		}
-//		return i;
-//}
-
-/*******************************************************************************
-	函数名：fputc
-	输  入:
-	输  出:
-	功能说明：
-	重定义putc函数，这样可以使用printf函数从串口1打印输出
-*/
-int fputc(int ch, FILE *f)
+void serial_irq_init(uint8_t index, uart_irq_handler handler, uint32_t id)
 {
-		while(!LL_USART_IsActiveFlag_TXE(USART1)){};
-	  //LL_USART_ClearFlag_TC(UARTx); 
-		LL_USART_TransmitData8(USART1,ch);
-
-	return ch;
+	irq_handler = handler;
+	serial_irq_ids[index] = id;
 }
-#ifdef __cplusplus
-}
-#endif
 
-E_UART::E_UART(USART_TypeDef *UARTx,E_PinBase *PinTx,E_PinBase *PinRx)
+/**
+ *@brief    uart构造函数
+ *@param    USART_TypeDef *UARTx -- uart端口; PIN_ID PinTx -- Tx pin id; IN_ID PinRx -- rx pin id;
+ *@retval   NONE
+*/
+E_UART::E_UART(USART_TypeDef *UARTx,PIN_ID PinTx,PIN_ID PinRx)
 {
 		this->UARTx = UARTx;
-		Rx = PinRx;
-		Tx = PinTx;
+		Rx = new E_PinBase(PinRx);
+		Tx = new E_PinBase(PinTx);
 }
 
 void E_UART::begin(uint32_t speed)
@@ -85,9 +56,116 @@ void E_UART::begin(uint32_t speed)
 	LL_USART_SetBaudRate(UARTx, SystemCoreClock, LL_USART_OVERSAMPLING_16, speed);
 	/* (4) Enable USARTx **********************************************************/
 	LL_USART_Enable(UARTx);
+	serial_irq_init(0, (&E_UART::_irq_handler), (uint32_t)this);
 	/* Polling USART initialisation */
 	while ((!(LL_USART_IsActiveFlag_TEACK(UARTx))) || (!(LL_USART_IsActiveFlag_REACK(UARTx))))
 	{
 	}
+}
+
+/**
+ *@name     size_t Uart::write(uint8_t c)
+ *@brief    串口方式发送一个字节
+ *@param    ch：要发送的字符
+ *@retval   已发送的数据
+*/
+size_t E_UART::write(uint8_t ch)
+{
+	while (!LL_USART_IsActiveFlag_TXE(UARTx)){};//单字节等待，等待寄存器空
+	LL_USART_TransmitData8(UARTx,ch);
+	return 1;
+}
+
+/**
+ *@name     uint16_t USART::read()
+ *@brief    串口接收数据，此函数只能在用户接收中断中调用
+ *@param    NONE
+ *@retval   串口所接收到的数据
+*/
+uint16_t E_UART::read()
+{
+    return (uint16_t)(UARTx->RDR & (uint16_t)0x01FF);
+}
+
+void E_UART::enable_irq(IrqType type){
+	// 附加中断回掉函数时，打开中断
+	NVIC_SetPriority(USART1_IRQn, 0);
+	NVIC_EnableIRQ(USART1_IRQn);
+	LL_USART_EnableIT_TC(UARTx);
+	/*ENABLE USART RXNE interrupts */
+	type == RxIrq?LL_USART_EnableIT_RXNE(UARTx):LL_USART_EnableIT_TC(UARTx);
+}
+
+void E_UART::disable_irq(IrqType type){
+	/*DISABLE USART RXNE interrupts */
+	type == RxIrq?LL_USART_DisableIT_RXNE(UARTx):LL_USART_DisableIT_TC(UARTx);
+}
+
+void E_UART::attach(void (*fptr)(void), IrqType type) {
+    if (fptr) {
+        _irq[type].attach(fptr);
+    }
+}
+
+void E_UART::_irq_handler(uint32_t id, IrqType irq_type) {
+    E_UART *handler = (E_UART*)id;
+    handler->_irq[irq_type].call();
+}
+
+extern "C" {
+	// 串口1中断
+	void USART1_IRQHandler(void)
+	{
+		if (LL_USART_IsActiveFlag_RXNE(USART1) == SET )
+		{
+			// 调用接收中断回调函数
+			irq_handler(serial_irq_ids[0],RxIrq);
+			// if (usart_callback_table[0][0] != 0)
+				// usart_callback_table[0][0]();
+			// 如果回调函数中没有读取数据，则将当前数据抛弃，准备下一次接收
+			if (LL_USART_IsActiveFlag_RXNE(USART1) == SET )
+			{
+				LL_USART_RequestRxDataFlush(USART1);
+			}
+		}
+		if (LL_USART_IsActiveFlag_TC(USART1) == SET)
+		{
+			// 清除忙标志，调用发送结束回调函数
+			//busy[0] = 0;
+			// if (usart_callback_table[0][1] != 0)
+				// usart_callback_table[0][1]();
+			irq_handler(serial_irq_ids[0],TcIrq);
+			// 清除发送结束中断标志
+			LL_USART_ClearFlag_TC(USART1);
+//			LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_2);
+		}
+	}
+
+#if !(defined(STM32F030x6)||defined(STM32F031x6))
+//	void USART2_IRQHandler(void)
+//	{
+//		if (LL_USART_IsActiveFlag_RXNE(USART2) == SET )
+//		{
+//			// 调用接收中断回调函数
+//			if (usart_callback_table[1][0] != 0)
+//				usart_callback_table[1][0]();
+//			// 如果回调函数中没有读取数据，则将当前数据抛弃，准备下一次接收
+//			if (LL_USART_IsActiveFlag_RXNE(USART2) == SET )
+//			{
+//				LL_USART_RequestRxDataFlush(USART2);
+//			}
+//		}
+//		if (LL_USART_IsActiveFlag_TC(USART2) == SET)
+//		{
+//			// 清除忙标志，调用发送结束回调函数
+//			busy[1] = 0;
+//			if (usart_callback_table[1][1] != 0)
+//				usart_callback_table[1][1]();
+//			// 清除发送结束中断标志
+//			LL_USART_ClearFlag_TC(USART2);
+//			LL_DMA_DisableChannel(DMA1, LL_DMA_CHANNEL_4);
+//		}
+//	}
+#endif
 }
 
--- mcu/stm32f0x/ebox/Peripherals/ebox_uart.h
+++ mcu/stm32f0x/ebox/Peripherals/ebox_uart.h
@@ -4,12 +4,13 @@
   * @author  cat_li
   * @version V2.0
   * @date    2016/10/21
-  * @brief   
+  * @brief
+	1  2017/5/25  移除E_PinBase的构造函数，使用PIN_ID
   ******************************************************************************
   * @attention
   *
-  * No part of this software may be used for any commercial activities by any form 
-  * or means, without the prior written consent of shentq. This specification is 
+  * No part of this software may be used for any commercial activities by any form
+  * or means, without the prior written consent of shentq. This specification is
   * preliminary and is subject to change at any time without notice. shentq assumes
   * no responsibility for any errors contained herein.
   * <h2><center>&copy; Copyright 2015 shentq. All Rights Reserved.</center></h2>
@@ -22,13 +23,61 @@
 #include "stm32_define.h"
 #include "ebox_define.h"
 #include "ebox_gpio.h"
+#include "print.h"
+#include "ebox_Template.h"
 
+#define UART_NUM (2)
 
+enum IrqType {
+	RxIrq = 0,
+	TcIrq
+};
+	
+typedef void (*uart_irq_handler)(uint32_t id, IrqType type);
 
-class E_UART{
+class E_UART: public Print{
 public:
-	E_UART(USART_TypeDef *UARTx,E_PinBase *PinTx,E_PinBase *PinRx);
+//	E_UART(USART_TypeDef *UARTx,E_PinBase *PinTx,E_PinBase *PinRx);
+
+	E_UART(USART_TypeDef *UARTx,PIN_ID PinTx,PIN_ID PinRx);
+
+	// initial uart
 	void begin(uint32_t speed);
+	void enable_irq(IrqType type);
+	void disable_irq(IrqType type);
+
+	//write method
+	virtual size_t  write(uint8_t ch);
+//	virtual size_t  write(const uint8_t *buffer, size_t size);
+	using   Print::write;
+
+	//read method
+	uint16_t read();
+	void    wait_busy();
+	/** Attach a function to call whenever a serial interrupt is generated
+	 *
+	 *  @param fptr A pointer to a void function, or 0 to set as none
+	 *  @param type Which serial interrupt to attach the member function to (Seriall::RxIrq for receive, TxIrq for transmit buffer empty)
+	 */
+	//attach user event
+	// 绑定静态回调函数
+	void attach(void (*fptr)(void), IrqType type);
+
+
+	/** Attach a member function to call whenever a serial interrupt is generated
+	 *
+	 *  @param tptr pointer to the object to call the member function on
+	 *  @param mptr pointer to the member function to be called
+	 *  @param type Which serial interrupt to attach the member function to (Seriall::RxIrq for receive, TxIrq for transmit buffer empty)
+	 */
+	template<typename T>
+	void attach(T* tptr, void (T::*mptr)(void), IrqType type) {
+		if ((mptr != NULL) && (tptr != NULL)) {
+			_irq[type].attach(tptr, mptr);
+		}
+	}
+
+
 
 private:
 	USART_TypeDef *UARTx;
@@ -36,6 +85,11 @@ private:
 	E_PinBase *Tx;
 
 	uint8_t index;
+
+	static void _irq_handler(uint32_t id, IrqType irq_type);
+
+protected:
+	FunctionPointer _irq[2];
 };
 
 #endif
