1eafd81e6eb9e1260aed79e17c7887687fb2c8e0
好心情 <cat_li8953@qq.com>
2017-07-12 22:29:30 +0800

    adc例程

--- example/2.analog/3.analog_read--F0.cpp
+++ example/2.analog/3.analog_read--F0.cpp
@@ -16,32 +16,29 @@
   ******************************************************************************
   */
 /**
- * -LQM (2016/9/19)
+ * -LQM (2017/7/12)
  *     注意：此部分和F1系列不通用，提供了2中ADC模式，一种是单通单次采集，不适用DMA
  *  一种连续采集（指定采样次数，单通道或多通道） 
- *  单通道单次采集,参考电压定义在analog.h中 VDDA_APPLI 
- *    1  声明对象   Analog  adc1(&PA0)
- *    2  提供read()和read_Voltage()方法
- *  连续采集
- *    1  声明对象(需要同时指定每通道采样的次数)  AnalogDMA adcs(2)
- *    2  添加采集通道  Add(&PA1);Add(&PA2);......
- *    3  取值前更新数据  update()
- *    4  取值  adcs.buffer[]
+ *  单通道单次采集,参考电压定义在ebox_analog.h中 VDDA_APPLI 
+ *    1  声明对象   E_Analog adc1(new E_PinBase(PA_0));
+ *    2  提供read()和getVoltage()方法
  */
 
 #include "ebox.h"
 
-Analog adc1(&PA0);
-AnalogDMA adcs(2);
+E_UART usart(USART1,PA_9,PA_10);
+
+E_Analog adc1(new E_PinBase(PA_0));
+E_AnalogDMA adcs(new E_PinBase(PA_0));
 
 void setup()
 {
     ebox_init();
-    uart1.begin(115200);
-	  uart1.printf("****ADC TEST ****** \r\n");
+    usart.begin(115200);
+	  usart.printf("****ADC TEST ****** \r\n");
 	  
-	  adcs.Add(&PA1);
-	  adcs.Add(&PA2);
+//	  adcs.Add(&PA1);
+//	  adcs.Add(&PA2);
 }
 
 int main(void)
@@ -49,18 +46,13 @@ int main(void)
     setup();
     while(1)
     {
-			  uart1.printf("\r\n****ADC TEST ONE ****** \r\n");
-        uart1.printf("PA0 = %d \r\n",adc1.read());
-			  uart1.printf("PA0 Voltage = %d mv \r\n",adc1.read_voltage());
+			  usart.printf("\r\n****ADC TEST ONE ****** \r\n");
+        usart.printf("PA0 = %d \r\n",adc1.read());
+			  usart.printf("PA0 Voltage = %d mv \r\n",adc1.getVoltage());
         delay_ms(2000);
-			  uart1.printf("****ADC TEST CONTINUOUS****** \r\n");
-			  adcs.update();
-			  uart1.printf("PA1_1 = %d  PA1_2 = %d  PA2_1 = %d  PA2_2 = %d \r\n",adcs.Buffer[0],adcs.Buffer[2],adcs.Buffer[1],adcs.Buffer[3]);
+			  usart.printf("****ADC TEST CONTINUOUS****** \r\n");
+//			  adcs.update();
+//			  usart.printf("PA1_1 = %d  PA1_2 = %d  PA2_1 = %d  PA2_2 = %d \r\n",adcs.Buffer[0],adcs.Buffer[2],adcs.Buffer[1],adcs.Buffer[3]);
         delay_ms(2000);
 		}
 }
-
-
-
-
-
